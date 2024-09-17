0b933202687e3d9af19ff9ceee261d4dadc05dac
Salih SARI <salsar41@gmail.com>
2024-09-17 15:38:04 +0300

    Screenshot file fix

--- .config/hypr/scripts/screenshots.sh
+++ .config/hypr/scripts/screenshots.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-grimblast --notify copysave area ~/Pictures/Screenshots/$(date +'%s_screenshot.png') 
+[ ! -d ~/Pictures/Screenshots ] && mkdir -p ~/Pictures/Screenshots
+grimblast --notify copysave area ~/Pictures/Screenshots/$(date +'%s_screenshot.png')
